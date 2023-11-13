c211c4ace35bccdac9c16ee3b0b462bdb5c07734
rihapfirdaus <muhammadrihap448@gmail.com>
2023-11-13 18:49:06 +0700

    update again

--- script.js
+++ script.js
@@ -2,11 +2,30 @@
 const imageNames = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 
 const sliderItems = imageNames.concat(imageNames);
-const slider = document.getElementById("slide-track");
+const sliders = document.querySelectorAll(".slide-track");
 
-sliderItems.forEach((sliderItem) => {
-  const img = document.createElement("img");
-  img.src = "./assets/" + sliderItem; //path to your folder image
-  img.className = "slide-item";
-  slider.appendChild(img);
+sliders.forEach((slider, index) => {
+  slider.innerHTML = ""; // Kosongkan isi slider sebelum menambahkan gambar
+
+  sliderItems.forEach((sliderItem) => {
+    const img = document.createElement("img");
+    img.src = "./assets/" + sliderItem; //path to your folder image
+    img.className = "slide-item";
+    slider.appendChild(img);
+  });
 });
+
+// Use the code below if you only want to create one slider
+
+/*
+  const sliderItems = imageNames.concat(imageNames);
+
+  const slider = document.getElementById("slide-track");
+
+  sliderItems.forEach((sliderItem) => {
+    const img = document.createElement("img");
+    img.src = "./assets/" + sliderItem; //path to your folder image
+    img.className = "slide-item";
+    slider.appendChild(img);
+  });
+*/
